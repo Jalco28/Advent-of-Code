5c4a0a1d4b58b3f9be7cb03d3593f4bc478e27ef
James Alcock <75587027+Jalco28@users.noreply.github.com>
2022-12-07 10:02:37 +0000

    Compact

--- Day6.py
+++ Day6.py
@@ -1,27 +1,22 @@
 with open('Day6Input.txt', 'r') as f:
     line = f.readline()
 
-def detect_uniques(string):
-    if len(set(string)) == 4:
+def detect_uniques(string, length):
+    if len(set(string)) == length:
         return True
     else:
         return False
 for i in range(len(line)):
     string = line[i:i+4]
-    if detect_uniques(string):
+    if detect_uniques(string, 4):
         print(string)
         print(i+4)
         break
-# Part 2
-def detect_uniques(string):
-    if len(set(string)) == 14:
-        return True
-    else:
-        return False
 
+# Part 2
 for i in range(len(line)):
     string = line[i:i+14]
-    if detect_uniques(string):
+    if detect_uniques(string, 14):
         print(string)
         print(i+14)
         break
