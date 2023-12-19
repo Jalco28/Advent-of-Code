5c38ef6120b163ec535c80e3be467c09b3be33c2
Jalco28 <75587027+Jalco28@users.noreply.github.com>
2023-12-19 17:52:49 +0000

    Day 12 p2 2023

--- 2023/Day12.py
+++ 2023/Day12.py
@@ -59,9 +59,55 @@ for idx, line in enumerate(copy(data)):
     data[idx] = [chars, tuple(digits)]
 
 total = 0
-for line in data:
-    working_combinations = list(all_possible(line[0], line[1]))
-    total += len(working_combinations)
+# for line in data:
+#     working_combinations = list(all_possible(line[0], line[1]))
+#     total += len(working_combinations)
 
+
+print(total)
+
+# Part 2
+
+@cache
+def solve(chars: str, digits: tuple[int], current_block):
+    if not chars:  # We've run out
+        if len(digits) == 0 and current_block == 0:
+            return 1
+        elif len(digits) == 1 and digits[0] == current_block:
+            return 1
+        return 0
+
+    if len(digits) > 0 and current_block > digits[0]:
+        return 0  # Current block already to big
+    if len(digits) == 0 and current_block > 0:
+        return 0  # Making a block that isn't needed
+
+    result = 0
+    next_char = chars[0]
+    if next_char in '#?':
+        result += solve(chars[1:], digits, current_block+1)
+    if next_char in '.?':
+        if not current_block:
+            result += solve(chars[1:], digits, 0)
+        elif current_block == digits[0]:
+            result += solve(chars[1:], digits[1:], 0)
+
+    return result
+
+
+def unfold(chars: str, digits: tuple[int]):
+    chars = '?'.join([chars]*5)
+    digits = list(digits)
+    digits.extend(digits*4)
+    return chars, tuple(digits)
+
+
+edata = []
+for line in data:
+    edata.append(list(unfold(line[0], line[1])))
+
+total = 0
+for chars, digits in edata:
+    total += solve(chars, digits, 0)
 
 print(total)
