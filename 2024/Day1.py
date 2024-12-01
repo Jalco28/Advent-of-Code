943652d1d58a5a6b66c3b8a025d5a13b75f4eb88
Jalco28 <75587027+Jalco28@users.noreply.github.com>
2024-12-01 12:57:23 +0000

    Day 1 2024 (here we go again)

---
+++ 2024/Day1.py
@@ -0,0 +1,17 @@
+with open('inputs/Day1.txt', 'r') as f:
+    data = [list(map(int, line.split('   '))) for line in f.readlines()]
+
+list1 = [data[i][0] for i in range(len(data))]
+list2 = [data[i][1] for i in range(len(data))]
+
+compare = zip(sorted(list1), sorted(list2))
+total = 0
+for a, b in compare:
+    total += abs(a-b)
+print(total)
+
+#Part 2
+similarity = 0
+for item in list1:
+    similarity += item*list2.count(item)
+print(similarity)
