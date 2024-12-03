f23e882848784beb68523fc5079a4107f462f182
Jalco28 <75587027+Jalco28@users.noreply.github.com>
2024-12-03 15:23:05 +0000

    compact part 1 and 2

--- 2024/Day3.py
+++ 2024/Day3.py
@@ -2,8 +2,17 @@
 with open('inputs/Day3.txt', 'r') as f:
     data = f.read()
 
-counter = 0
+counter_p1 = 0
+counter_p2 = 0
+enabled = True
+
 for start_index in range(len(data)):
+    if data[start_index:start_index+7] == 'don\'t()':
+        enabled = False
+        continue
+    if data[start_index:start_index+4] == 'do()':
+        enabled = True
+        continue
     if data[start_index:start_index+4] != 'mul(':
         continue
 
@@ -26,40 +35,9 @@ for start_index in range(len(data)):
         continue
     num2[1] = current_index-1
 
-    counter += int(data[num1[0]:num1[1]+1]) * int(data[num2[0]:num2[1]+1])
-print(counter)
+    counter_p1 += int(data[num1[0]:num1[1]+1]) * int(data[num2[0]:num2[1]+1])
+    if enabled:
+        counter_p2 += int(data[num1[0]:num1[1]+1]) * int(data[num2[0]:num2[1]+1])
 
-# Part 2
-enabled = True
-counter = 0
-for start_index in range(len(data)):
-    if data[start_index:start_index+7] == 'don\'t()':
-        enabled = False
-        continue
-    if data[start_index:start_index+4] == 'do()':
-        enabled = True
-        continue
-    if data[start_index:start_index+4] != 'mul(' or not enabled:
-        continue
-
-    current_index = start_index+4
-    num1 = [current_index, None]  # Index range of nums
-    num2 = [None, None]
-
-    while data[current_index].isdecimal():
-        current_index += 1
-    num1[1] = current_index-1
-
-    if data[current_index] != ',':
-        continue
-    current_index += 1
-
-    num2[0] = current_index
-    while data[current_index].isdecimal():
-        current_index += 1
-    if data[current_index] != ')':
-        continue
-    num2[1] = current_index-1
-
-    counter += int(data[num1[0]:num1[1]+1]) * int(data[num2[0]:num2[1]+1])
-print(counter)
+print(counter_p1)
+print(counter_p2)
